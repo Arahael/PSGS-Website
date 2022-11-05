a1fde17f011f375ff2b1b764701b8658c4ed18d6
Arahael <maciej.kornecki@gmail.com>
2022-11-05 08:22:20 +0100

    Add files via upload

---
+++ aktaoperatorów.js
@@ -0,0 +1,159 @@
+
+
+
+
+/*
+function load_op1(){
+            document.getElementById("aktabody").innerHTML='<object type="type/html" data="operator1.html" ></object>';
+  }
+function load_op2(){
+            document.getElementById("aktabody").innerHTML='<object type="type/html" data="operator2.html" ></object>';
+  }
+function load_op3(){
+            document.getElementById("aktabody").innerHTML='<object type="type/html" data="operator3.html" ></object>';
+  }
+function load_op4(){
+            document.getElementById("aktabody").innerHTML='<object type="type/html" data="operator4.html" ></object>';
+  }  
+function load_op5(){
+            document.getElementById("aktabody").innerHTML='<object type="type/html" data="operator5.html" ></object>';
+  }
+function load_op6(){
+            document.getElementById("aktabody").innerHTML='<object type="type/html" data="operator6.html" ></object>';
+  }
+function load_op7(){
+            document.getElementById("aktabody").innerHTML='<object type="type/html" data="operator7.html" ></object>';
+  }
+function load_op8(){
+            document.getElementById("aktabody").innerHTML='<object type="type/html" data="operator8.html" ></object>';
+  }
+function load_op9(){
+            document.getElementById("aktabody").innerHTML='<object type="type/html" data="operator9.html" ></object>';
+  }
+function load_op10(){
+            document.getElementById("aktabody").innerHTML='<object type="type/html" data="operator10.html" ></object>';
+  }
+function load_op11(){
+            document.getElementById("aktabody").innerHTML='<object type="type/html" data="operator11.html" ></object>';
+  }
+function load_op12(){
+            document.getElementById("aktabody").innerHTML='<object type="type/html" data="operator12.html" ></object>';
+  }
+function load_op13(){
+            document.getElementById("aktabody").innerHTML='<object type="type/html" data="operator13.html" ></object>';
+  }
+function load_op14(){
+            document.getElementById("aktabody").innerHTML='<object type="type/html" data="operator14.html" ></object>';
+  }
+function load_op15(){
+            document.getElementById("aktabody").innerHTML='<object type="type/html" data="operator15.html" ></object>';
+  }
+function load_op16(){
+            document.getElementById("aktabody").innerHTML='<object type="type/html" data="operator16.html" ></object>';
+  }  
+function load_op17(){
+            document.getElementById("aktabody").innerHTML='<object type="type/html" data="operator17.html" ></object>';
+  }
+function load_op18(){
+            document.getElementById("aktabody").innerHTML='<object type="type/html" data="operator18.html" ></object>';
+  }
+function load_op19(){
+            document.getElementById("aktabody").innerHTML='<object type="type/html" data="operator19.html" ></object>';
+  }
+function load_op20(){
+            document.getElementById("aktabody").innerHTML='<object type="type/html" data="operator20.html" ></object>';
+  }
+function load_op21(){
+            document.getElementById("aktabody").innerHTML='<object type="type/html" data="operator21.html" ></object>';
+  }
+function load_op22(){
+            document.getElementById("aktabody").innerHTML='<object type="type/html" data="operator22.html" ></object>';
+  }
+function load_op23(){
+            document.getElementById("aktabody").innerHTML='<object type="type/html" data="operator23.html" ></object>';
+  }
+function load_op24(){
+            document.getElementById("aktabody").innerHTML='<object type="type/html" data="operator24.html" ></object>';
+  }
+function load_op25(){
+            document.getElementById("aktabody").innerHTML='<object type="type/html" data="operator25.html" ></object>';
+  }    
+
+function load_op26(){
+            document.getElementById("aktabody").innerHTML='<object type="type/html" data="operator26.html" ></object>';
+  }
+function load_op27(){
+            document.getElementById("aktabody").innerHTML='<object type="type/html" data="operator27.html" ></object>';
+  }
+function load_op28(){
+            document.getElementById("aktabody").innerHTML='<object type="type/html" data="operator28.html" ></object>';
+  }
+function load_op29(){
+            document.getElementById("aktabody").innerHTML='<object type="type/html" data="operator29.html" ></object>';
+  }  
+function load_op30(){
+            document.getElementById("aktabody").innerHTML='<object type="type/html" data="operator30.html" ></object>';
+  }
+function load_op31(){
+            document.getElementById("aktabody").innerHTML='<object type="type/html" data="operator31.html" ></object>';
+  }
+function load_op32(){
+            document.getElementById("aktabody").innerHTML='<object type="type/html" data="operator32.html" ></object>';
+  }
+function load_op33(){
+            document.getElementById("aktabody").innerHTML='<object type="type/html" data="operator33.html" ></object>';
+  }
+function load_op34(){
+            document.getElementById("aktabody").innerHTML='<object type="type/html" data="operator34.html" ></object>';
+  }
+function load_op35(){
+            document.getElementById("aktabody").innerHTML='<object type="type/html" data="operator35.html" ></object>';
+  }
+function load_op36(){
+            document.getElementById("aktabody").innerHTML='<object type="type/html" data="operator36.html" ></object>';
+  }
+function load_op37(){
+            document.getElementById("aktabody").innerHTML='<object type="type/html" data="operator37.html" ></object>';
+  }
+function load_op38(){
+            document.getElementById("aktabody").innerHTML='<object type="type/html" data="operator38.html" ></object>';
+  }
+function load_op39(){
+            document.getElementById("aktabody").innerHTML='<object type="type/html" data="operator39.html" ></object>';
+  }
+function load_op40(){
+            document.getElementById("aktabody").innerHTML='<object type="type/html" data="operator40.html" ></object>';
+  }
+function load_op41(){
+            document.getElementById("aktabody").innerHTML='<object type="type/html" data="operator41.html" ></object>';
+  }  
+function load_op42(){
+            document.getElementById("aktabody").innerHTML='<object type="type/html" data="operator42.html" ></object>';
+  }
+function load_op43(){
+            document.getElementById("aktabody").innerHTML='<object type="type/html" data="operator43.html" ></object>';
+  }
+function load_op44(){
+            document.getElementById("aktabody").innerHTML='<object type="type/html" data="operator44.html" ></object>';
+  }
+function load_op45(){
+            document.getElementById("aktabody").innerHTML='<object type="type/html" data="operator45.html" ></object>';
+  }
+function load_op46(){
+            document.getElementById("aktabody").innerHTML='<object type="type/html" data="operator46.html" ></object>';
+  }
+function load_op47(){
+            document.getElementById("aktabody").innerHTML='<object type="type/html" data="operator47.html" ></object>';
+  }
+function load_op48(){
+            document.getElementById("aktabody").innerHTML='<object type="type/html" data="operator48.html" ></object>';
+  }
+function load_op49(){
+            document.getElementById("aktabody").innerHTML='<object type="type/html" data="operator49.html" ></object>';
+  }
+function load_op50(){
+            document.getElementById("aktabody").innerHTML='<object type="type/html" data="operator50.html" ></object>';
+  }    
+
+
+  */
